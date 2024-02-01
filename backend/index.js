016f0f680733028d6915b26fc8e8ec199d23e852
unknown <hagartalbi@gmail.com>
2024-02-02 00:10:32 +0100

    setup ,db cnx and Book model added

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,37 @@
 import express from 'express'
 import 'dotenv/config'
 import mongoose from 'mongoose'
+import {Book} from './models/bookModel.js'
 const app = express()
 
 app.get('/',(req,res)=>{
     console.log(req)
     return res.status(200).send("welcome")
+})
+
+app.post('/books',async(req,res)=>{
+    try{
+
+        if(!req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear){
+                return res.status(400).send({message : 'Send all required fields'})
+        }
+        else {
+            const newBook = {
+                title : req.body.title,
+                author : req.body.author,
+                publishYear : req.body.publishYear,
+
+            }
+            const book = await Book.create(newBook)
+            return res.status(201).send(book)
+        }
+
+    }catch(err){
+        console.log(err.message)
+        res.status(500).send({message : err.message})
+    }
 })
 
 
